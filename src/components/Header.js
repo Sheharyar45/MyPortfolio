b74c0e93de592c750bc2893581cd695f99b250d8
Sheharyar45 <meghanisheheryar@gmail.com>
2024-08-02 17:31:56 +0500

    ss

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
             aria-current="page"
             to="/"
           >
-            About Me haha
+            About Me
           </Link>
         </li>
         <li>
